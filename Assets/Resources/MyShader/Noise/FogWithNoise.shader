4d5422e2155e7a59f8e1b70c65c3692e93d8bcc2
mangobass <mangobass@users.noreply.github.com>
2017-08-10 14:57:07 +0800

    添加基于noise texture的雾效处理

--- Assets/Resources/MyShader/Noise/FogWithNoise.shader
+++ Assets/Resources/MyShader/Noise/FogWithNoise.shader
@@ -1,41 +1,103 @@
-﻿Shader "Custom/FogWithNoise" {
+﻿Shader "MyShader/Noise/FogWithNoise" {
 	Properties {
-		_Color ("Color", Color) = (1,1,1,1)
-		_MainTex ("Albedo (RGB)", 2D) = "white" {}
-		_Glossiness ("Smoothness", Range(0,1)) = 0.5
-		_Metallic ("Metallic", Range(0,1)) = 0.0
+		_MainTex("Base (RGB)", 2D) = "white" {}
+		_FogDensity("Fog Density", Float) = 1.0
+		_FogColor("Fog Color", Color) = (1,1,1,1)
+		_FogStart("Fog Start", Float) = 0.0
+		_FogEnd("Fog End", Float) = 1.0
+		_NoiseTex("Noise Texture", 2D) = "white" {}
+		_FogXSpeed("Fog Horizental Speed", Float) = 0.1
+		_FogYSpeed("Fog Vertical Speed", Float) = 0.1
+		_NoiseAmount("Noise Amount", Float) = 1
 	}
+
 	SubShader {
-		Tags { "RenderType"="Opaque" }
-		LOD 200
-		
-		CGPROGRAM
-		// Physically based Standard lighting model, and enable shadows on all light types
-		#pragma surface surf Standard fullforwardshadows
-
-		// Use shader model 3.0 target, to get nicer looking lighting
-		#pragma target 3.0
-
+		CGINCLUDE
+		#include "UnityCG.cginc"
 		sampler2D _MainTex;
+		half4 _MainTex_TexelSize;
+		sampler2D _CameraDepthTexture;
+		float _FogDensity;
+		float4 _FogColor;
+		float _FogStart;
+		float _FogEnd;
+		sampler2D _NoiseTex;
+		half _FogXSpeed;
+		half _FogYSpeed;
+		half _NoiseAmount;
 
-		struct Input {
-			float2 uv_MainTex;
+		// 一次存放的方向：左下，右下，右上，左上
+		float4x4 _FrustumCornersRay;
+
+		struct v2f {
+			float4 pos : SV_POSITION;
+			half2 uv : TEXCOORD0;
+			half2 uv_depth : TEXCOORD1;
+			// 插值后的像素向量
+			float4 interPloatedRay : TEXCOORD2;
 		};
 
-		half _Glossiness;
-		half _Metallic;
-		fixed4 _Color;
+		v2f vert(appdata_img v)
+		{
+			v2f o;
+			o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+			o.uv = v.texcoord;
+			o.uv_depth = v.texcoord;
 
-		void surf (Input IN, inout SurfaceOutputStandard o) {
-			// Albedo comes from a texture tinted by color
-			fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
-			o.Albedo = c.rgb;
-			// Metallic and smoothness come from slider variables
-			o.Metallic = _Metallic;
-			o.Smoothness = _Glossiness;
-			o.Alpha = c.a;
+			#if UNITY_UV_STARTS_AT_TOP
+			if (_MainTex_TexelSize.y < 0)
+				o.uv_depth.y = 1 - o.uv_depth.y;
+			#endif
+
+			int index = 0;
+			if (v.texcoord.x < 0.5 && v.texcoord.y < 0.5)
+			{
+				index = 0;
+			}
+			else if (v.texcoord.x > 0.5 && v.texcoord.y < 0.5)
+			{
+				index = 1;
+			}
+			else if (v.texcoord.x > 0.5 && v.texcoord.y > 0.5)
+			{
+				index = 2;
+			}
+			else
+			{
+				index = 3;
+			}
+			#if UNITY_UV_STARTS_AT_TOP
+			if (_MainTex_TexelSize.y < 0)
+				index = 3-index;
+			#endif
+
+			o.interPloatedRay = _FrustumCornersRay[index];
+			return o;
+		}
+
+		fixed4 frag(v2f i) : SV_Target
+		{
+			float linearDepth = LinearEyeDepth(SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv_depth));
+			float3 worldPos = _WorldSpaceCameraPos + linearDepth*i.interPloatedRay.xyz;
+
+			float2 speed = _Time.y * float2(_FogXSpeed, _FogYSpeed);
+			float noise = (tex2D(_NoiseTex, i.uv+speed).r-0.5f) * _NoiseAmount;
+
+			float fogDensity = (_FogEnd - worldPos.y) / (_FogEnd - _FogStart);
+			fogDensity = saturate(fogDensity * _FogDensity * (1+noise));
+			fixed4 color = tex2D(_MainTex, i.uv);
+			color.rgb = lerp(color.rgb, _FogColor, fogDensity);
+			return color;
 		}
 		ENDCG
+
+		Pass {
+			ZTest Always Cull Off ZWrite Off
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			ENDCG
+		}
 	}
-	FallBack "Diffuse"
+	Fallback Off
 }
---
+++ Assets/Scripts/PostEffectsSample/FogWithNoiseTexture.cs
@@ -0,0 +1,120 @@
+﻿//=============================================================================
+/// Author : Peng Jiawei
+/// FileName : FogWithNoiseTexture.cs
+//=============================================================================
+using UnityEngine;
+using System.Collections;
+using ShaderScript.PostEffect;
+
+namespace Sample.PostEffect {
+	public class FogWithNoiseTexture : PostEffectBase
+	{
+		public Shader FogShader;
+		private Material m_FogMaterial;
+
+		public Material UsedMaterial
+		{
+			get
+			{
+				m_FogMaterial = CheckShaderAndCreateMaterial(FogShader, m_FogMaterial);
+				return m_FogMaterial;
+			}
+		}
+
+		public Camera MyCamera { set; get; }
+		private Transform CameraTrans { set; get; }
+
+		[Range(0.0f, 3.0f), Header("浓度值")]
+		public float m_FogDensity = 1.0f;
+		public Color m_FogColor = Color.white;
+
+		[Header("起始高度")]
+		public float m_FogStart = 0.0f;
+		[Header("结束高度")]
+		public float m_FogEnd = 2.0f;
+
+		public Texture m_NoiseTexture;
+
+		[Range(-0.5f, 0.5f)]
+		public float m_FogXSpeed = 0.1f;
+		[Range(-0.5f, 0.5f)]
+		public float m_FogYSpeed = 0.1f;
+
+		/// <summary>
+		/// 控制噪声的程度
+		/// 0: 一个均匀的基于高度的全局雾效
+		/// </summary>
+		[Range(0.0f, 3.0f)] public float m_NoiseAmount = 1.0f;
+
+		private void Awake()
+		{
+			MyCamera = GetComponent<Camera>();
+			CameraTrans = MyCamera.transform;
+		}
+
+		private void OnEnable()
+		{
+			MyCamera.depthTextureMode |= DepthTextureMode.Depth;
+		}
+
+		private void OnRenderImage(RenderTexture src, RenderTexture dest)
+		{
+			if (UsedMaterial != null)
+			{
+				Matrix4x4 frustumCorners = Matrix4x4.identity;
+
+				float fov = MyCamera.fieldOfView;
+				float near = MyCamera.nearClipPlane;
+				float far = MyCamera.farClipPlane;
+				float aspect = MyCamera.aspect;
+
+				float halfHeight = near * Mathf.Tan(fov * 0.5f * Mathf.Deg2Rad);
+				Vector3 toRight = CameraTrans.right * halfHeight * aspect;
+				Vector3 toTop = CameraTrans.up * halfHeight;
+
+				Vector3 topLeft = CameraTrans.forward * near + toTop - toRight;
+				float scale = topLeft.magnitude / near;
+				topLeft.Normalize();
+				topLeft *= scale;
+
+				Vector3 topRight = CameraTrans.forward * near + toTop + toRight;
+				topRight.Normalize();
+				topRight *= scale;
+
+				Vector3 buttomLeft = CameraTrans.forward * near - toTop - toRight;
+				buttomLeft.Normalize();
+				buttomLeft *= scale;
+
+				Vector3 buttomRight = CameraTrans.forward * near - toTop + toRight;
+				buttomRight.Normalize();
+				buttomRight *= scale;
+
+				// 存放顺序会影响shader的代码
+				frustumCorners.SetRow(0, buttomLeft);
+				frustumCorners.SetRow(1, buttomRight);
+				frustumCorners.SetRow(2, topRight);
+				frustumCorners.SetRow(3, topLeft);
+
+				UsedMaterial.SetMatrix("_FrustumCornersRay", frustumCorners);
+				//UsedMaterial.SetMatrix("_ViewProjInvMatrix", (MyCamera.projectionMatrix*MyCamera.worldToCameraMatrix).inverse);
+
+				UsedMaterial.SetFloat("_FogDensity", m_FogDensity);
+				UsedMaterial.SetColor("_FogColor", m_FogColor);
+				UsedMaterial.SetFloat("_FogStart", m_FogStart);
+				UsedMaterial.SetFloat("_FogEnd", m_FogEnd);
+
+				UsedMaterial.SetTexture("_NoiseTex", m_NoiseTexture);
+				UsedMaterial.SetFloat("_FogXSpeed", m_FogXSpeed);
+				UsedMaterial.SetFloat("_FogYSpeed", m_FogYSpeed);
+				UsedMaterial.SetFloat("_NoiseAmount", m_NoiseAmount);
+
+				Graphics.Blit(src, dest, UsedMaterial);
+			}
+			else
+			{
+				Graphics.Blit(src, dest);
+			}
+		}
+	}
+}
+// EOF

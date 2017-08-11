0ad057fa10d45d30df3331dd59c134053fb70cdd
mangobass <mangobass@users.noreply.github.com>
2017-08-11 21:21:19 +0800

    EditWindows test function update

--- Assets/Scripts/Editor/EditorWindowTest.cs
+++ Assets/Scripts/Editor/EditorWindowTest.cs
@@ -19,10 +19,10 @@ namespace SelfFunction
 
 		private void OnGUI()
 		{
-			GUILayout.TextField("Test editor window.", GUILayout.Width(200), GUILayout.Height(30));
-			if (GUILayout.Button("Btn", GUILayout.Width(100), GUILayout.Height(40)))
+			GUILayout.Label("Test editor window.");
+			//GUILayout.TextField("Test editor window.", GUILayout.Width(200), GUILayout.Height(30));
+			if (GUILayout.Button("Click", GUILayout.Width(100), GUILayout.Height(40)))
 			{
-				//GUILayout.TextField(string.Format("Btn clicked num: {0}", ++ClickNum), GUILayout.Width(200), GUILayout.Height(30));
 				Debug.LogFormat("Btn clicked num: {0}", ++ClickNum);
 			}
 		}
